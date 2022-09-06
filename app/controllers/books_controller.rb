f9d28ccf8cda85cb75fe8aaeb66aecb55c30ae2a
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-09-06 05:35:01 +0000

    [add]Task_2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get '/top' => 'homes#top'
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
