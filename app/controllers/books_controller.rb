6fafccb244ffeac75485d74712485a4e3a077798
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-09-07 01:44:30 +0000

    [add]Task_2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,12 +4,26 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    def new
+      @book = Book.new
+    end
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def show
   end
 
   def edit
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
   end
 
   private
